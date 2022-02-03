fa66709076cf465930c71c48d9d9f0390cb83cdd
1142052Sebastian <1142052@itisrossi.vi.it>
2022-02-03 08:51:42 +0100

    Documentazione
    Test creazione documentazione documentazione Doxygen

---
+++ html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['es_20tpsit_0',['Es TPSIT',['../md__r_e_a_d_m_e.html',1,'']]]
+];
--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@
 
 using namespace std;
 
-
+/** 
+ *  @file   Stack.cpp
+ *  @brief  Esercizio per creazione stack 
+ *  @author Burs Raul Sebastian	 
+ *  @date   03/08/2022 
+ ***********************************************/
+ 
+ 
 // Da svolgere SOLO in linguaggio C++ COMMENTANDO adeguatamente il codice.
 
 
@@ -23,9 +30,17 @@ using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+/** @brief
+* Questa e' una struc e serve per poter creare nuovi oggetti da mettere in stack
+* @retval Val e' il valore memorizzato nella cella dello stack
+* @retval *Next invece e' un puntatore che punta alla cella di memoria di Cell
+*
+*/
+
 struct Cell{
 	char val;
 	Cell *next;
+
 };
 
 
@@ -33,6 +48,9 @@ int main(int argc, char** argv)
 {
 	// Crea un nuovo Cell con variabile a e un puntatore che punta ad a
 	// In output a.val == *p.val -> Entrambi puntano alla stessa memoria 
+	/** @param Root Questa e' il parametro che determina la partenza dello stack
+	* 
+	**/
 	Cell Root;
 	Cout<< "Inserisci un carattere da mettere in coda, altrimenti inserisci 1 - ";
 	Cin >> int se;
@@ -41,7 +59,8 @@ int main(int argc, char** argv)
 		Cell a;	
 		Cell Root.next = Cell
 	}
-
+/// Brief description which ends at this dot. Details follow
+/// here.
 	cin>> a.val;
 	cout<< a.val;
 	Cell *p = &a;
